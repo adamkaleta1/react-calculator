0220f04cf5bb59b8c1ac8aaffeb8cb47aa02bd6d
User <adam.kaleta97@gmail.com>
2025-02-13 20:53:30 +0100

    calculator

---
+++ src/DigitButton.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types"
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, digit }) {
+    return (
+        <button
+            onClick={() =>
+                dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })
+            }
+        >
+            {digit}
+        </button>
+    )
+};
+
+DigitButton.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    digit: PropTypes.string.isRequired
+}
---
+++ src/OperationButton.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types"
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, operation }) {
+    return (
+        <button
+            onClick={() =>
+                dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+            }
+        >
+            {operation}
+        </button>
+    )
+};
+
+DigitButton.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    operation: PropTypes.string.isRequired
+}
